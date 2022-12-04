7970aa2596f66e9ba4024d96b96278da775aafd1
yunsanch <60577975+yunsanch@users.noreply.github.com>
2022-12-04 11:55:08 -0500

    add
    adding

---
+++ sastreilor/SignInSignUp/SignInVC.swift
@@ -0,0 +1,37 @@
+//
+//  SignInVC.swift
+//  sastreilor
+//
+//  Created by Yunior Sanchez on 12/4/22.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
